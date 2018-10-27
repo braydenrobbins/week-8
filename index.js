67a6b7c1dbeb6b1fb132fc34464960c7c11b5eb3
Brayden Robbins <braydenrobbins98@gmail.com>
2018-10-27 09:36:06 -0500

    Set up an address book array

---
+++ index.js
@@ -0,0 +1,20 @@
+var addressBook = [
+    {
+        firstname: "Brayden",
+        lastname: "Robbins",
+        phone: "4051238532",
+        address: "123 bakers field"
+    },
+    {
+        firstname: "Trae",
+        lastname: "Robbins",
+        phone: "9128374422",
+        address: "4622 Main St"
+    },
+    {
+        firstname: "Bob",
+        lastname: "Rockers",
+        phone: "3243458723",
+        address: "8732 Weenie Hut Way" 
+    }
+];
